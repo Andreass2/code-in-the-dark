625ce881b79130a0b84c7f8b69304ee8ce339f2d
Andreas Hammerbeck <andreas.hammerbeck@variant.no>
2024-08-08 11:32:03 +0200

    fix pages

---
+++ config/event2.tsx
@@ -0,0 +1,24 @@
+export const eventId = 1;
+export const eventName = "Code in the dark Variant edition";
+export const reference_image = "/page.png";
+export const instructions = `
+--- The rules ---
+1) No previews - of either results or assets!
+2) Stay in this editor at all times
+3) No measurement tools
+4) Stop coding when the time's up
+5) After the round is over, press "Finish"
+
+Good luck and most important of all; have fun!
+
+--- Assets ---
+/bt-logo.png (not in correct size)- bt logo
+/sparkesykkel.png (not in correct size)- column 1 image
+/pastasentralen.png (not in correct size)- column 2 image
+/brannspiller.png (not in correct size)- column 3 image
+Light blue color: #EBF2FE;
+`;
+
+export const injectCode = `<link rel="stylesheet"  href="https://unpkg.com/@dfds-frontend/fonts@0.0.4/main/font.css"  />`
+export const showPreview = false;
+export const demoMode = true;
---
+++ config/event3.tsx
@@ -0,0 +1,22 @@
+export const eventId = 1;
+export const eventName = "Code in the dark Variant edition";
+export const reference_image = "/page.png";
+export const instructions = `
+--- The rules ---
+1) No previews - of either results or assets!
+2) Stay in this editor at all times
+3) No measurement tools
+4) Stop coding when the time's up
+5) After the round is over, press "Finish"
+
+Good luck and most important of all; have fun!
+
+--- Assets ---
+/torghatten-logo.png - Torghatten logo
+/banner.jpg - banner image
+Light blue color: #E6F2FA;
+`;
+
+export const injectCode = `<link rel="stylesheet"  href="https://unpkg.com/@dfds-frontend/fonts@0.0.4/main/font.css"  />`
+export const showPreview = false;
+export const demoMode = true;
